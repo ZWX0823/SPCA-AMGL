38ba968ad6d786b833df2ab47d2a386409ca68ec
ZWX0823 <39873541+ZWX0823@users.noreply.github.com>
2022-03-13 17:10:28 +0800

    Update Optimize_W.py

--- Optimize_W.py
+++ Optimize_W.py
@@ -1,34 +1,40 @@
 import numpy as np
 from numpy.random import rand
 
-def Optimize_W(X, W_init, S, d, alpha, beta, s):
+def Optimize_W(X, W_init, S, d, lamda1, lamda2, s):
 
+    # The bands nums
     b = X.shape[1]
+    # Laplace
     D = np.diag(np.sum(S, axis=1))
     L = D - S
+    # Transform 2,1-norm into trace operation
     row, col = np.diag_indices(b)
     Q = np.zeros([b,b])
     Q[row,col] = np.array([1 / np.linalg.norm(w) for w in W_init])
-    A = -X.T @ X + alpha * Q + beta * X.T @ L @ X
+
+    A = -X.T @ X + lamda1 * Q + lamda2 * X.T @ L @ X
     if s==0:
-        alpha_A = np.max(np.abs(np.linalg.eig(A)[0]))
+        lamda1_A = np.max(np.abs(np.linalg.eig(A)[0]))
     elif s==1:
         ww = rand(b,1)
         for i in range(20):
             m1 = A @ ww
             q = m1 / np.linalg.norm(m1)
             ww = q
-        alpha_A = np.abs(ww.T @ A @ ww)
+        lamda1_A = np.abs(ww.T @ A @ ww)
     else:
         print('Warning: error input!!!')
-        return;
-    
+        return
+        
+    # Our goal is to maximize the value of trace(W.T @ A @ W) and we find that its value reaches the maximum at the first cycle
     t = 0
     t_max = 1                            
-    A_til = alpha_A * np.eye(b) - A
+    A_til = lamda1_A * np.eye(b) - A
     T = np.zeros(t_max)
+    W = W_init
     while t < t_max:
-        M = A_til @ W_init
+        M = A_til @ W
         U, Sigma, VT = np.linalg.svd(M, full_matrices=0)
         W = U @ VT
         T[t] = np.trace(W.T @ A @ W)
